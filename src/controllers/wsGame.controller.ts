32bd9f81b27f07d64831f77fbd3c4686acaadc6d
fvan-wij <vanwijkflip@gmail.com>
2025-04-22 18:47:38 +0200

    Improved network code

--- src/controllers/wsGame.controller.ts
+++ src/controllers/wsGame.controller.ts
@@ -34,7 +34,6 @@ export async function addPlayer(request: FastifyRequest, reply: FastifyReply) {
 
   if (!players.get(userPackage.id)) {
     if (userPackage.x === 0 && userPackage.y === 0) {
-      // 34, 23 spawn location
       players.set(userPackage.id, { x: 36, y: 23 })
     } else {
       players.set(userPackage.id, { x: userPackage.x, y: userPackage.y })
@@ -57,47 +56,70 @@ export async function removePlayer(request: FastifyRequest, reply: FastifyReply)
 
 
 async function syncPlayersDB() {
-  try {
-    const response = await fetch(`${DATABASE_URL}/game/players`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(players)
-    });
-    if (!response.ok)
+  console.log("Syncing players...", players);
+  if (!players || players.size === 0)
   {
-      console.log("PUT request was not ok!");
-      throw new Error("Wtf, PUT REQUEST WAS BAD");
+    console.log("Nothing to sync, no players...");
+    setTimeout(syncPlayersDB, 5000);
+    return;
+  }
+
+  try {
+
+    //TODO: sync players in a batch.
+    // const response = await fetch(`${DATABASE_URL}/game/players`, {
+    //   method: 'PUT',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify(players)
+    // });
+    // if (!response.ok)
+    // {
+    //   throw {code: 500, message: "Failed to upate user"};
+    // }
+
+    for (const [id, position] of players) 
+    {
+      const response = await fetch(`${DATABASE_URL}/game/players/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(position)
+      });
+      if (!response.ok)
+      {
+        throw {code: 500, message: "Failed to upate user"};
+      }
     }
-  } catch (error) {
+    console.log("Succesfully synced players!");
+  } catch (error)
+  {
       console.error(`sync db error`, error);
   }
+  setTimeout(syncPlayersDB, 10000);
 }
 
-// const timeoutId = setTimeout(() => updatePosition(5, {x: 20, y: 20}), 5000);
+syncPlayersDB();
 
 export async function wsGameController(client: WebSocket, request: FastifyRequest) {
+
+
   clients.add(client);
   let playerId : number = -1;
 
   client.on('message', async (message) => {
     const data = JSON.parse(message.toString());
-    console.log("(On message) Server received: ", data);
 
     if (data.type === "newConnection") {
+      console.log("(On message) Server received: ", data);
       playerId = data.id;
-      // Client initializes itself in the game and sends player data to the server
-      // We receive player data and add it to the players map
       players.set(data.id, { x: data.position.x, y: data.position.y });
-      // We notifiy other players that a new connection has been made, other clients add new player to local map
       broadcast({ type: "newPlayer", id: data.id, username: data.username, avatar: data.avatar, position: data.position }, client);
-      // console.log("Broadcasting initializePlayers", players);
       if (client.readyState == 1) {
         client.send(JSON.stringify({ type: "initializePlayers", players: Array.from(players.entries()) }));
       }
     }
 
     if (data.type === "move") {
-      // console.log("Broadcasting move");
+      players.set(data.id, {x: data.position.x, y: data.position.y});
       broadcast(data, client);
     }
   });
@@ -108,17 +130,17 @@ export async function wsGameController(client: WebSocket, request: FastifyReques
     let id = playerId;
     try {
       let pos = players.get(id);
-      // const response = await fetch(`${DATABASE_URL}/game/players/${id}`, {
-      //   method: 'PUT',
-      //   headers: { 'Content-Type': 'application/json' },
-      //   body: JSON.stringify(pos)
-      // });
-      // if (!response.ok)
-      // {
-      //   console.log("PUT request was not ok!");
-      //   throw new Error("Wtf, PUT REQUEST WAS BAD");
-      // }
       broadcast({ type: "disconnectPlayer", id: id }, client);
+      const response = await fetch(`${DATABASE_URL}/game/players/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(pos)
+      });
+      if (!response.ok)
+      {
+        throw {code: 500, message: "Failed to upate user"};
+      }
+      console.log("Succesfully synced players!");
       console.log(`Disconnecting ${id} from server`);
       clients.delete(client);
       players.delete(id);
@@ -129,5 +151,4 @@ export async function wsGameController(client: WebSocket, request: FastifyReques
     }
 
   });
-
 };
